950796afe44a5eb1a7a424530bc3be8dbc3898ec
ethil <nidinkrish@gmail.com>
2020-12-21 14:44:09 +0530

    name calculator added

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 (function(){
-    'use strict'
-    angular.module(testapp,[])
+'use strict';
+    angular.module('testapp',[])
 
     .controller('testcontroller',function($scope){
-        $scope.name="nitthya";
+ //       $scope.name="nittya";
+ //       $scope.sayhello=function(){
+ //           return "coursera";
+ //       };
+
+        $scope.name="";
+        $scope.totalvalue=0;
+
+        $scope.displaynumeric=function (){
+            var totalnamevalue=calculatenumericforstring($scope.name);    //get total value
+            $scope.totalvalue=totalnamevalue;
+        };
+        function calculatenumericforstring(String){
+            var totalstringvalue=0;
+            for(var i=0 ; i < String.length ; i++){
+                totalstringvalue += String.charCodeAt(i);
+            }
+            return totalstringvalue;
+        }
+
     });
 
 })();
